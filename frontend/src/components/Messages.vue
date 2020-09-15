67912f3045d1355ecee770f5c92d936f6b229bbd
Federico Barraza <federico.barraza@valtech.com>
2020-09-14 21:04:44 -0300

    initial commit

---
+++ frontend/src/components/Messages.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h4>Messages</h4>
+      <ul v-for="message in messages" :key="message">
+          <li>{{message}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            messages: [ "Hello", "Hi", "It's working"]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
