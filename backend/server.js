72865ae99a4b934a8b643530de3117212db854b3
Federico Barraza <fede.barraza@outlook.com>
2020-09-20 19:17:23 -0300

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
+
+let messages = [ "Hello", "Yes", "It's working"];
 
 app.get('/messages', (req, res) => {
-    const messages = [ "Hello", "Yes", "It's working"];
     res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+    let msg = req.body;
+    messages.push(msg.message);
+    res.send(msg);
 });
 
 app.listen(port, () => console.log('app running'));
